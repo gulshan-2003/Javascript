0c284d19c91b7adacc1cc3417070c10eaafde3bf
gulshan-2003 <gulshantagore2003@gmail.com>
2025-08-24 10:21:16 +0530

    I have written code to know about the document and window

---
+++ DOM/script.js
@@ -0,0 +1,11 @@
+let a = document.body.children;
+let b = document.body.children[0];
+let c = document.body.children[0].textContent;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// window.alert("Hii");
+console.log(window.innerWidth);
+console.log(window.document == document);
